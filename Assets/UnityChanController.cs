1bff64c2d581388f70fa3a38e0d7f05410e085a4
Marihuana <aisatu_nomahou@yahoo.co.jp>
2018-09-10 21:53:14 +0900

    refact script

--- Assets/UnityChanController.cs
+++ Assets/UnityChanController.cs
@@ -1,77 +1,57 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
-public class UnityChanController : MonoBehaviour
-{
+public class UnityChanController : MonoBehaviour {
     public float Speed = 2f;
     public float Thrust = 100;
 
     private Rigidbody _rigidbody;
     private Animator _animator;
 
-    private bool _onGround;
+    private bool _onGround ;
 
-    void Start()
-    {
+    void Start () {
         _rigidbody = GetComponent<Rigidbody>();
         _animator = GetComponent<Animator>();
     }
 
-    void Update()
-    {
+    void Update () {
         if (!_onGround) return;
         CalcMove();
         CalcJump();
     }
 
-    private void CalcMove()
-    {
-        if (Input.GetKey(KeyCode.RightArrow))
-        {
+    private void CalcMove() {
+        if (Input.GetKey(KeyCode.RightArrow)) {
             _rigidbody.velocity = new Vector3(Speed, 0, 0);
             transform.rotation = Quaternion.Euler(0, 90, 0);
-        }
-        else if (Input.GetKey(KeyCode.LeftArrow))
-        {
+        } else if (Input.GetKey(KeyCode.LeftArrow)) {
             _rigidbody.velocity = new Vector3(-Speed, 0, 0);
             transform.rotation = Quaternion.Euler(0, 270, 0);
-        }
-        else if (Input.GetKey(KeyCode.UpArrow))
-        {
+        } else if (Input.GetKey(KeyCode.UpArrow)) {
             _rigidbody.velocity = new Vector3(0, 0, Speed);
             transform.rotation = Quaternion.Euler(0, 0, 0);
-        }
-        else if (Input.GetKey(KeyCode.DownArrow))
-        {
+        } else if (Input.GetKey(KeyCode.DownArrow)) {
             _rigidbody.velocity = new Vector3(0, 0, -Speed);
             transform.rotation = Quaternion.Euler(0, 180, 0);
-        }
-        else
-        {
+        } else {
             _animator.SetBool("Running", false);
             return;
         }
-
+                    
         _animator.SetBool("Running", true);
     }
 
-    private void CalcJump()
-    {
-        if (Input.GetKey(KeyCode.Space))
-        {
+    private void CalcJump() {
+        if (Input.GetKey(KeyCode.Space)) {
             _rigidbody.AddForce(new Vector3(0, Thrust, 0));
             _animator.SetBool("Jumping", true);
             _onGround = false;
-        }
-        else
-        {
+        } else {
             _animator.SetBool("Jumping", false);
         }
     }
 
-    void OnCollisionStay(Collision col)
-    {
+    void OnCollisionStay(Collision col) {
         _onGround = true;
     }
 }
